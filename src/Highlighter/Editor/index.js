3c9d64638f99adc9568ab44b884a6766ae4e8b79
Illia Olenchenko <icrosil@gmail.com>
2018-10-13 21:06:45 +0300

    highlighter: added single selection

--- src/Highlighter/Editor/index.js
+++ src/Highlighter/Editor/index.js
@@ -1,51 +1,92 @@
 import React, { Component } from 'react';
-import { Editor as SlateEditor } from 'slate-react';
-import { Value } from 'slate';
 
+import { Editor, findDOMRange } from 'slate-react';
+import initialValue from './initialValue';
 import './Editor.css';
 
-// TODO separate state from view
-const initialValue = Value.fromJSON({
-  document: {
-    nodes: [
-      {
-        object: 'block',
-        type: 'paragraph',
-        nodes: [
-          {
-            object: 'text',
-            leaves: [
-              {
-                text: 'A line of text in a paragraph.',
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-});
-
-class Editor extends Component {
+class RichTextEditor extends Component {
   state = {
     value: initialValue,
   };
 
-  // On change, update the app's React state with the new editor value.
-  onChange = ({ value }) => {
-    this.setState({ value });
+  hasMark = type => {
+    const { value } = this.state;
+    return value.activeMarks.some(mark => mark.type === type);
   };
+
+  ref = editor => {
+    this.editor = editor;
+  };
+
   render() {
     const { value } = this.state;
     return (
-      <section className="editor">
+      <div className="editor">
         <h3>Simple Rich Editor</h3>
-        <SlateEditor onChange={this.onChange} value={value} className="slate" />
-      </section>
+        <div className="toolbar">
+          {this.renderMarkButton('code', 'highlight')}
+        </div>
+        <Editor
+          autoFocus
+          placeholder="Enter some rich text..."
+          ref={this.ref}
+          value={value}
+          onChange={this.onChange}
+          onSelect={this.onSelect}
+          renderMark={this.renderMark}
+          className="slate"
+        />
+      </div>
     );
   }
+
+  renderMarkButton = (type, icon) => {
+    const isActive = this.hasMark(type);
+
+    // TODO show somehow is current mode active and which selection it is
+    return (
+      <button
+        active={isActive}
+        onMouseDown={event => this.onClickMark(event, type)}
+      >
+        {icon}
+      </button>
+    );
+  };
+
+  renderMark = (props, next) => {
+    const { children, mark, attributes } = props;
+
+    switch (mark.type) {
+      case 'code':
+        return <code {...attributes}>{children}</code>;
+      default:
+        return next();
+    }
+  };
+
+  onChange = ({ value }) => {
+    this.setState({ value });
+  };
+
+  onSelect = (event, { value }, next) => {
+    const domRange = findDOMRange(value.selection);
+    const { startOffset, endOffset } = domRange;
+    console.log(startOffset, 'startOffset');
+    console.log(endOffset, 'endOffset');
+    console.log(domRange, 'rest');
+    next();
+  };
+
+  onClickMark = (event, type) => {
+    event.preventDefault();
+
+    this.editor.change(change => {
+      change.toggleMark(type);
+    });
+  };
 }
 
-Editor.propTypes = {};
+RichTextEditor.propTypes = {};
 
-export default Editor;
+export default RichTextEditor;
---
+++ src/Highlighter/Editor/initialValue.js
@@ -0,0 +1,24 @@
+import { Value } from 'slate';
+
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            leaves: [
+              {
+                text: 'A line of text in a paragraph.',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+});
+
+export default initialValue;
